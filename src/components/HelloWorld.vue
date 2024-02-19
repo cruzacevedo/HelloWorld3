6554f484f16082451a1a8d32d92e39ff37716a99
cruzacevedo <140022289+cruzacevedo@users.noreply.github.com>
2024-02-19 13:48:11 -0600

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1> Informacion Per: {{ info }} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      info: ['Karina','20961','5A']
+    }
+  }
+}
+</script>
